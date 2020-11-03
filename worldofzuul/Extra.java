7d47a22425255d221e48436c4bf59d4baba918a3
Jonas Kaad <jonaskaad@gmail.com>
2020-11-03 12:09:29 +0100

    Changed ENUM; GLUTEN -> CONTAINS_GLUTEN

--- worldofzuul/Extra.java
+++ worldofzuul/Extra.java
@@ -2,7 +2,7 @@ package worldofzuul;
 
 public enum Extra {
     VEGAN("Vegan"), CONTAINS_LACTOSE("Contains Lactose"), CONTAINS_PEANUTS("Contains Peanuts"),
-    ORGANIC("Organic"), GLUTEN("Gluten"), CONTAINS_SOY("Contains Soy");
+    ORGANIC("Organic"), CONTAINS_GLUTEN("Contains Gluten"), CONTAINS_SOY("Contains Soy");
 
     private String extraWord;
 
