3d117b08408054f4ffd66aaab0473dc3f0a7d0ac
Alexander N <AlexanderNorup@users.noreply.github.com>
2020-11-03 12:31:47 +0100

    Update worldofzuul/Parser.java

--- worldofzuul/Parser.java
+++ worldofzuul/Parser.java
@@ -20,7 +20,11 @@ public class Parser {
         System.out.print("> ");
 
         inputLine = reader.nextLine();
-
+       //Uses substring to cut the input into the first and second word.
+       //substring works like this. The first argument is the start-character (indexed from 0), and the second argument is the length (which is also optional).
+       //That means if you call .substring(2) on the String "Hello" you get the String "llo", because the new string is like the previous string, but it now starts from the 3rd character. 
+       //The second argument says how many characters after the start-character the new string should continue. So calling .substring(2,2) would output "ll", because it starts from the 3rd character and reads 2 characters forward.
+       //The indexOf() method on a String, will return an int which represents the character position of the first occurrence of a given String. So if you call indexOf("ll") on "Hello", you'll get 2, because "ll" first occurs in "Hello" on the 3rd character
         if(inputLine.contains(" ")){
             word1 = inputLine.substring(0, inputLine.indexOf(' '));
             word2 = inputLine.substring(inputLine.indexOf(' ')+1);
