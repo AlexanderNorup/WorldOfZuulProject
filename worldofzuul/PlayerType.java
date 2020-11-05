9438c24a7950b284a9cd1c7aedc1a68cc87b3a5e
TKalo <troelskaldau@live.dk>
2020-11-05 13:08:08 +0100

    happiness calculator v1 .....

--- worldofzuul/PlayerType.java
+++ worldofzuul/PlayerType.java
@@ -1,21 +1,23 @@
 package worldofzuul;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PlayerType {
 
     private String name;
-    private double proteinGoal;
+    private String description;
+    private double budgetMax;
     private double calorieMin;
     private double calorieGoal;
-    private double budgetMax;
+    private double proteinFactor;
     private double budgetFactor;
-    private double pickiness;
+    private double pickynessfactor;
     private ArrayList<Item> faveItems;
     private ArrayList<Item> hateItems;
     private ArrayList<Extra> thingsThatMatter;
 
-    public PlayerType(String name) {
+    public PlayerType(String name, String description) {
         this.name = name;
         this.faveItems = new ArrayList<>();
         this.hateItems = new ArrayList<>();
@@ -23,6 +25,10 @@ public class PlayerType {
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription(){
+        return description;
     }
 
     public double getCalorieMin() {
@@ -33,14 +39,17 @@ public class PlayerType {
         return budgetMax;
     }
 
-    public void setProteinFactor(double proteinGoal) { this.proteinGoal = proteinGoal; }
-
-    public void setBudgetFactor(double budgetFactor) {
-        this.budgetFactor = budgetFactor;
+    public void setlimitations(int budgetMax, int calorieMin){
+        this.budgetMax = budgetMax;
+        this.calorieMin = calorieMin;
     }
 
-    public void setPickiness(double pickiness) {
-        this.pickiness = pickiness;
+    //set factors to determin happiness caluclation for playertype
+    public void setFactors(int proteinFactor, int budgetFactor, int pickynessFactor){
+        int equalizer = 100/(proteinFactor + budgetFactor + pickynessFactor);
+        this.proteinFactor = proteinFactor * equalizer;
+        this.budgetFactor = budgetFactor * equalizer;
+        this.pickynessfactor = pickynessFactor * equalizer;
     }
 
     public void addFaveItems(Item faveItem) {
@@ -64,8 +73,11 @@ public class PlayerType {
         double totalCalories = 0.0;
         double totalProtein = 0.0;
         int itemsSatisfaction = 0;
-        ArrayList<Item> varietyList = new ArrayList<>();
-        double variety = 10;
+        int faveItemsBought = 0;
+        int hateItemsBaught = 0;
+        int percentItemsContainingExtras = 0;
+        double variety = 0;
+        ArrayList<Item> itemTypeList = new ArrayList<>();
 
         for (Item item : items) {
 
@@ -76,27 +88,63 @@ public class PlayerType {
 
             // Calculates satisfaction based on favorite and hated items
             if (faveItems.contains(item)) {
-                itemsSatisfaction -= 1;
+                faveItemsBought += 1;
             }
             if (hateItems.contains(item)) {
-                itemsSatisfaction -= 2;
+                hateItemsBaught += 1;
             }
 
-            // calculates variety
-            if (varietyList.contains(item)) {
-                variety /= 1.5;
-            } else {
-                varietyList.add(item);
+            //check types of items in inventory
+            if(!itemTypeList.contains(item)){
+                itemTypeList.add(item);
+                variety += 1;
+            }
+
+            for(Extra extra : thingsThatMatter){
+                percentItemsContainingExtras += (Arrays.asList(item.getExtra()).contains(extra) ? 1 : 0) / thingsThatMatter.size();
             }
         }
 
-        // will add happiness if over proteinMin and subtract happiness if under proteinMin
-        happiness += ((totalProtein/proteinGoal)-1)*30;
-        happiness += ((totalCalories/calorieGoal)-1)*30;
-        happiness += itemsSatisfaction*pickiness;
-        happiness += (variety-5);
+        //Calculate budget happiness ( max 80)
+        if(totalPrice < budgetMax*0.8){
+            happiness += 80 * (1 - totalPrice/budgetMax*0.8);
+        }else {
+            happiness -= 80 * totalPrice/budgetMax*0.8;
+        }
 
-        return 0;
+        //Calculate protein happiness (max +80)
+        double minimumProtein = calorieGoal * 0.15 / 4;
+        double proteinGoal = calorieGoal * 0.15 / 4;
+        int tempHappiness = 0;
+
+        if(totalProtein > minimumProtein){
+            tempHappiness += 80 * (totalProtein-minimumProtein)/(proteinGoal-minimumProtein);
+        }else {
+            tempHappiness -= 80 * (1 - (totalProtein)/(minimumProtein));
+        }
+        happiness += Math.max(tempHappiness, 80);
+
+
+
+        //calculate pickyness
+        double temp1 = (double) faveItemsBought / (double) faveItems.size();
+        double temp2 = (double) hateItemsBaught / (double) hateItems.size();
+        double temp3 = (temp1+percentItemsContainingExtras)/2;
+
+        happiness += 80 * temp3 * pickynessfactor;
+        happiness += 80 * temp2 * pickynessfactor;
+
+        //generic happiness
+        //Calorie, variaty
+        happiness -= (1 - (totalCalories - calorieMin) / (calorieGoal - calorieMin)) * 10;
+
+        if(variety > 5){
+            happiness += Math.max((variety - 5) * 2,10);
+        }else {
+            happiness -= (variety - 5) * 2;
+        }
+
+        return happiness;
     }
 
 
