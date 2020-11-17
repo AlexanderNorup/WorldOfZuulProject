ea04b6a0bdd63e37869d4050f04c1c649814883e
Jonas Kaad <jonaskaad@gmail.com>
2020-11-17 13:56:23 +0100

    Added a main menu with music

--- src/main/java/worldofzuul/MainGUI.java
+++ src/main/java/worldofzuul/MainGUI.java
@@ -4,21 +4,35 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.Enumeration;
 
 public class MainGUI extends Application {
+    Scene s;
+    MediaPlayer mediaPlayer;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         //Pathen til resourcen er relativt fra /src/main/resources/
-        Parent root = FXMLLoader.load(MainGUI.class.getResource("/fxml/test.fxml"));
+        Parent root = FXMLLoader.load(MainGUI.class.getResource("/fxml/mainmenu.fxml"));
+        s = new Scene(root, 1197,720);
 
-        Scene s = new Scene(root, 300,275);
+        String music = "/music/mario.mp3";
+        File file = new File(music);
+        System.out.println(file.exists());
+        Media media = new Media(MainGUI.class.getResource("/music/pilfinger.mp3").toString());  //plays music
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+
         primaryStage.setScene(s);
+        primaryStage.setResizable(false);
         primaryStage.setTitle("WorldOfZuul Test");
+
         primaryStage.show();
     }
 }
