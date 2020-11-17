47fbe828140b1ec9b4969996cb962db1d217402f
TKalo <troelskaldau@live.dk>
2020-11-17 12:03:02 +0100

    Edited SideMenu controller

--- src/main/java/worldofzuul/SideMenuController.java
+++ src/main/java/worldofzuul/SideMenuController.java
@@ -1,4 +1,37 @@
 package worldofzuul;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import worldofzuul.Objects.Item;
+
+import java.util.ArrayList;
+
 public class SideMenuController {
+
+    @FXML
+    public Label SideMenuTitleLabel;
+
+    @FXML
+    public Label SideMenuBudgetLabel;
+
+    @FXML
+    public Label SideMenuCalorieLabel;
+
+    @FXML
+    public Label SideMenuProteinLabel;
+
+    @FXML
+    public ListView<Item> SideMenuListView;
+
+    private ObservableList<Item> observableItems;
+    public void setSideMenuListView(ArrayList<Item> items){
+
+        observableItems.addAll(items);
+        SideMenuListView.setItems(observableItems);
+    }
+
+
+
 }
