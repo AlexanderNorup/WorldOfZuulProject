8d076b68b945121a67390d069942d69a9f100bff
Magnus <magnus.kjaer310101@gmail.com>
2020-11-24 11:03:22 +0100

    Better abstraction to data layer

---
+++ src/main/java/worldofzuul/DataLayer/ISaveGame.java
@@ -0,0 +1,8 @@
+package worldofzuul.DataLayer;
+
+import java.util.ArrayList;
+
+public interface ISaveGame {
+    void save(ArrayList<GameResultData> gameResults) throws SaveGameException;
+    ArrayList<GameResultData> load() throws SaveGameException;
+}
--- src/main/java/worldofzuul/DataLayer/SaveFile.java
+++ src/main/java/worldofzuul/DataLayer/SaveFile.java
@@ -3,9 +3,6 @@ package worldofzuul.DataLayer;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import worldofzuul.DomainLayer.ContentGenerator;
-import worldofzuul.DomainLayer.GameResult;
-import worldofzuul.DomainLayer.PlayerType;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -19,77 +16,88 @@ import java.util.Scanner;
  * The data is an Arraylist of GameResults
  * A GameResult contains a PlayerType - only the name of this is saved to the file.
  */
-public class SaveFile {
-    private static final String path = "./saveFile.json";
+public class SaveFile implements ISaveGame {
+    private String path;
 
     private static final String CO2_STRING = "co2";
     private static final String HAPPINESS_STRING = "happiness";
     private static final String PLAYERTYPE_STRING = "playerType";
 
+    public SaveFile(String path){
+        this.path = path;
+    }
+
     /**
      * Saves the given Arraylist to a JSON file
      * Will create the JSON file if it doesn't already exist
-     * Throws a FileNotFoundException if the path is somehow broken,
-     * but I don't se how that would happen since the path is relative
-     * I don't se how a JSONException would be thrown either
-     * @param gameResults The data to be saved
-     * @throws FileNotFoundException
-     * @throws JSONException
+     * @param gameResults
+     * @throws SaveGameException JSON or FileNotFound-Exceptions will be chained to a SaveGameException and thrown
      */
-    public static void save(ArrayList<GameResult> gameResults) throws FileNotFoundException, JSONException {
+    public void save(ArrayList<GameResultData> gameResults) throws SaveGameException {
         JSONArray arr = new JSONArray();
-        for (GameResult g : gameResults) {
+        for (GameResultData g : gameResults) {
             JSONObject o = new JSONObject();
 
             //The data is put into the file as key-value pairs
-            o.put(CO2_STRING, g.getCo2());
-            o.put(HAPPINESS_STRING, g.getHappiness());
-            o.put(PLAYERTYPE_STRING, g.getPlayerType().getName());
+            try {
+                o.put(CO2_STRING, g.getCo2());
+                o.put(HAPPINESS_STRING, g.getHappiness());
+                o.put(PLAYERTYPE_STRING, g.getPlayerTypeName());
+            } catch (JSONException e){
+                throw new SaveGameException("JSON Exception when saving game", e);
+            }
 
             arr.put(o);
         }
-        PrintWriter printWriter = new PrintWriter(path);
-        printWriter.println(arr.toString());
-        printWriter.close();
+        try {
+            PrintWriter printWriter = new PrintWriter(path);
+            printWriter.println(arr.toString());
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            throw new SaveGameException("FileNotFoundException when saving game", e);
+        }
     }
 
     /**
      * Loads data from a saved file
-     * If there's no saved file, the method throws a FileNotFoundException
-     * Would be appropriate for the calling method to create an empty ArrayList in this case
-     *
-     * @return The ArrayList of Gameresults last saved to the file
-     * @throws FileNotFoundException
-     * @throws IOException
-     * @throws JSONException
+     * @return
+     * @throws SaveGameException JSON, IO or FileNotFound-Exceptions will be chained to a SaveGameException and thrown
      */
-    public static ArrayList<GameResult> load() throws FileNotFoundException, IOException, JSONException {
-        ArrayList<GameResult> toReturn = new ArrayList<>();
+    public ArrayList<GameResultData> load() throws SaveGameException{
+        ArrayList<GameResultData> toReturn = new ArrayList<>();
 
         File file = new File(path);
-        Scanner s = new Scanner(file);
 
-        // Throws an IOException if the file is empty
-        if (s.hasNextLine()) {
+        try {
+            Scanner s = new Scanner(file);
 
-            // Reads a string from the scanner and converts it into a jsonArray
-            // Throws a JSONException, if the contents of the JSON file is not an array
-            String jsonString = s.nextLine();
-            JSONArray arr = new JSONArray(jsonString);
+            // Throws an IOException if the file is empty
+            if (s.hasNextLine()) {
 
-            double co2;
-            double happiness;
-            PlayerType playerType;
-            for (int i = 0; i < arr.length(); i++) {
-                JSONObject o = (JSONObject) arr.get(i);
-                co2 = o.getDouble(CO2_STRING);
-                happiness = (double) o.getDouble(HAPPINESS_STRING);
-                playerType = ContentGenerator.getPlayerTypeByName(o.getString(PLAYERTYPE_STRING));
-                toReturn.add(new GameResult(co2, happiness, playerType));
+                // Reads a string from the scanner and converts it into a jsonArray
+                // Throws a JSONException, if the contents of the JSON file is not an array
+                String jsonString = s.nextLine();
+                JSONArray arr = new JSONArray(jsonString);
+
+                double co2;
+                double happiness;
+                String playerTypeName;
+                for (int i = 0; i < arr.length(); i++) {
+                    JSONObject o = (JSONObject) arr.get(i);
+                    co2 = o.getDouble(CO2_STRING);
+                    happiness = o.getDouble(HAPPINESS_STRING);
+                    playerTypeName = o.getString(PLAYERTYPE_STRING);
+                    toReturn.add(new GameResultData(co2, happiness, playerTypeName));
+                }
+
+            } else {
+                //Is caught immediately an chained with a SaveGameException
+                throw new IOException("Seems like the saveFile.json file exists but is empty");
             }
-
-        } else {
-            throw new IOException("Seems like the saveFile.json file exists but is empty");
+        } catch (IOException e) {
+            throw new SaveGameException("IOException when loading game", e);
+        } catch (JSONException e) {
+            throw new SaveGameException("JSONException when loading game", e);
         }
 
         return toReturn;
