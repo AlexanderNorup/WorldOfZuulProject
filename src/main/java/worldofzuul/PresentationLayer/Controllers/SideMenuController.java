5d6bdeec068edbda8c32a93a455c5daff1cb8c1d
Magnus <magnus.kjaer310101@gmail.com>
2020-12-03 14:08:45 +0100

    some string formatting in the sideMenu

--- src/main/java/worldofzuul/PresentationLayer/Controllers/SideMenuController.java
+++ src/main/java/worldofzuul/PresentationLayer/Controllers/SideMenuController.java
@@ -53,7 +53,7 @@ public class SideMenuController {
 
         moneyBar.setProgress(0);
         moneySpent.setText(Double.toString(0));
-        moneyGoal.setText(Double.toString(MainGUI.game.getPlayer().getBudget()));
+        moneyGoal.setText(String.format("%4.2f", MainGUI.game.getPlayer().getBudget()));
 
         ObservableList<IItem> listViewList = FXCollections.observableArrayList();
         listViewList.addAll(MainGUI.game.getPlayer().getInventory());
@@ -61,10 +61,10 @@ public class SideMenuController {
         listViewList.addListener(new ListChangeListener<IItem>() {
             @Override
             public void onChanged(Change<? extends IItem> c) {
-                sideMenuCalorieLabel.setText(Double.toString(MainGUI.game.getPlayer().getInventoryCalories()));
-                sideMenuProteinLabel.setText(Double.toString(MainGUI.game.getPlayer().getInventoryProtein()));
+                sideMenuCalorieLabel.setText(Integer.toString((int)(MainGUI.game.getPlayer().getInventoryCalories())));
+                sideMenuProteinLabel.setText(Integer.toString((int)(MainGUI.game.getPlayer().getInventoryProtein())) + " g");
                 moneyBar.setProgress(MainGUI.game.getPlayer().getInventoryValue()/MainGUI.game.getPlayer().getBudget());
-                moneySpent.setText(Double.toString(MainGUI.game.getPlayer().getInventoryValue()));
+                moneySpent.setText(String.format("%4.2f", MainGUI.game.getPlayer().getInventoryValue()));
             }
         });
 
