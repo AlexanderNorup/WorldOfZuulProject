ea32495df26c1412771d8527f756f7048c0ab48f
Jonas Kaad <jonaskaad@gmail.com>
2020-12-10 13:41:07 +0100

    Total co2 emitted added to result screen

--- src/main/java/worldofzuul/DomainLayer/Game.java
+++ src/main/java/worldofzuul/DomainLayer/Game.java
@@ -272,11 +272,12 @@ public class Game implements IGame {
             returnString.append("CO2: ").append(String.format("%4.2f", lastGame.getCo2())).append("\n");
             returnString.append("Happiness: ").append(String.format("%2.0f", lastGame.getHappiness())).append("\n\n\n");
             if(finishedGames.size() == 1) {
-                returnString.append("You have played: ").append(finishedGames.size()).append(" time. \n---\n");
+                returnString.append("You have played: ").append(finishedGames.size()).append(" time.\n");
             }
             else{
-                returnString.append("You have played: ").append(finishedGames.size()).append(" times. \n---\n");
+                returnString.append("You have played: ").append(finishedGames.size()).append(" times.\n");
             }
+            returnString.append("Total Co2 emitted: ").append(String.format("%4.2f",co2)).append("\n---\n");
         }
 
         //co2
